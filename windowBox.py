c58baaab7093d1a707691111972a2d59afa98508
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2024-11-03 19:14:16 +0000

    Update windowBox.py

--- windowBox.py
+++ windowBox.py
@@ -4,13 +4,13 @@ from PIL import ImageTk,Image
 # Root
 root = Tk()
 root.title("My Code")
-root.iconbitmap('c:/Users/corru/OneDrive/Documents/Python/GUI')
+root.iconbitmap('')
 
 def open():
     global theImage
     top = Toplevel()
     top.title("Second Window")
-    top.iconbitmap('c:/Users/corru/OneDrive/Documents/Python/GUI')
+    top.iconbitmap('')
     theImage = ImageTk.PhotoImage(Image.open("images/Elden_Ring.jpg"))
     aLabel = Label(top, image=theImage).pack()
     button2 = Button(top, text="Close", command=top.destroy).pack()
